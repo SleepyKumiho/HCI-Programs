864771d58eee99ee415cdef4d4140bd3ffec6e25
SleepyKumiho <113648451+SleepyKumiho@users.noreply.github.com>
2022-10-04 21:01:57 -0500

    Update reactionTime.py

--- reactionTime.py
+++ reactionTime.py
@@ -1,4 +1,3 @@
-import time
 """
 Writing out main thoughts for now. Will be deleted later as code is written
 
@@ -20,11 +19,39 @@ Likely need to add multiple input conditions using if statements for this to wor
 Possible food for thought: If the user presses enter too early, how do we stop them?
   Until this is programmed in, add to the starting text blurb saying: hey, we trust you not to cheat right now
 """
-print("Hello! This program will test your reaction time.")
-print("Before typing, please read the instructions carefully:")
-print("  1. If you wish to quit, please type 'q' and press enter")
-print("  2. When the test begins, there will be a pause that lasts anywhere from 0-10 seconds, \
-followed by a bunny appearing on screen. When you see the bunny, press enter as soon as possible.")
-print("  3. Your time will be printed on the screen, and all of your results from this session will be stored in a list")
-print("  4. To view the list, please type 'view'")
-print("  5. Finally, to begin the test, type 'start'")
+
+import time
+import random
+
+# Introductory text that provides info about how to use this program to the viewer
+intro_text = "Hello! This program will test your reaction time.\n Before typing, please read the following information carefully: \n \
+1. If you wish to quit, please type 'quit' and press enter \n 2. When the test begins, there will be a pause that lasts anywhere from 1-10 seconds, \
+followed by a bunny appearing on screen.\n   When you see the bunny, press enter as soon as possible \n \
+3. Your time will be printed on the screen, and all of your results from this session will be stored in a list.\
+    \n   To view the list, please type 'view' \n \
+5. To begin the test, type 'start' \n 6. Finally, for a list of all available commands, type 'help'"
+
+# A list of commands that can be printed with the right keyword
+commands = "To quit: 'quit' \n To start the test: 'start' \n To view previous times: 'view' \n To view the commands: 'help'"
+
+print(intro_text)
+
+# Will store user input for later
+value = ""
+# Will track how many times the test has been run
+counter = 0
+# List to store all previous times
+times = []
+
+# The interactive portion
+while value != "quit" :
+    value = input().lower()
+    if value == "help":
+        print(commands)
+    elif value == "view":
+        print(times)
+    elif value == "start":
+        counter += 1
+        print("Not implemented")
+        times.append(random.randint(1, 10))
+        print(f"The program has been run {counter} times")
